469709f2fe5e653984091901c9713768bec8bed8
Max <MRoslov@alfabank.ru>
2021-11-16 18:16:23 +0400

    delete config

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
-        <title>Create Next App</title>
+        <title>A blog by Max Roslow</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
